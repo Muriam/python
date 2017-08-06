ccbf1b009ff598d3543f9d1f68f93767acc85fdd
Muriam <d_muriam@inbox.ru>
2017-08-06 21:05:12 +0700

    first commit

---
+++ simple/lab1_math.py
@@ -0,0 +1,10 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> t=1
+>>> z=3
+>>> y=math.sin(t)
+>>> x=(4*y)**2/(4*z-(2*t)**3)
+>>> print (x)
+2.8322936730942847
+>>> 
