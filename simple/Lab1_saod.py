ecee417b019a706ea9fe0d4738ba4c4c68caabfb
Maria Duvanova <d_muriam@inbox.ru>
2018-11-18 21:38:07 +0700

    Update Lab1_saod.py

--- simple/Lab1_saod.py
+++ simple/Lab1_saod.py
@@ -1,8 +1,36 @@
 from random import randint
 
-def random_array(list):
-    list = [randint(1, 100) for _ in range(10)]
-    return list
+arr = [randint(1, 100) for _ in range(10)]
 
-arr = random_array(list)    
-print('original list\n', *arr)
+def bubble_sort():
+    for i in range(9):
+        for j in range(9 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+def select_sort():
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+def shaker_sort():
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        for i in range(left, right, +1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        right -= 1                
+        for i in range(right, left, -1):
+            if arr[i - 1] > arr[i]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+        left += 1
+        
+print('original array\n', *arr)
+#bubble_sort()    
+#print('bubble sort\n', *arr)
+#select_sort()               
+#print('selection sort\n', *arr)
+shaker_sort()               
+print('shaker sort\n', *arr) 
