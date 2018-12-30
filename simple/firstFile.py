b77f7ec3e1ba2b34fa483b50c83b49609c5483fb
Maria Duvanova <d_muriam@inbox.ru>
2018-12-30 23:47:31 +0700

    Update firstFile.py

--- simple/firstFile.py
+++ simple/firstFile.py
@@ -1,11 +1,10 @@
-#вывод файла на экран
-f = open ('text.txt', 'rw')
-print (f.read())
+#чтение файла и вывод на экран
 
-'''
-for line in f:      то же самое другим способом
-	print(line)
-
-f.write ('...')     записать что-то в файл, отображается в блокноте
-f.close()     
-'''
+with open('text.txt', 'r+', encoding='utf_8') as file:
+    data = []
+    data = list(file)
+    
+    for row in file.readlines():
+        data.append(row)
+	
+    print(*data)
