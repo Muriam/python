4fce334bd8df08afe70dd7e9e0a9e1d35148ccb6
Maria Duvanova <d_muriam@inbox.ru>
2018-12-31 13:10:28 +0700

    edit2

--- simple/firstFile.py
+++ simple/firstFile.py
@@ -1,10 +1,4 @@
 #чтение файла и вывод на экран
 
-with open('text.txt', 'r+', encoding='utf_8') as file:
-    data = []
-    data = list(file)
-    
-    for row in file.readlines():
-        data.append(row)
-	
-    print(*data)
+with open('text.txt', 'r') as file: 
+    print(file.read())
