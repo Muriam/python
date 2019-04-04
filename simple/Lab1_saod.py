093d08430ece2d9bba2dcf2fd2d89c0aeb351aad
Maria Duvanova <d_muriam@inbox.ru>
2019-04-04 19:57:40 +0700

    counting comparisons and permutations in bubble sort

--- simple/Lab1_saod.py
+++ simple/Lab1_saod.py
@@ -1,12 +1,20 @@
+# отсортировать список: пузырьковой, методом прямого выбора, и шейкерной сортировками. Подчситать кол-во сравнений и пересылок.
 from random import randint
+comparison = 0
+transfer = 0
+
 
 arr = [randint(1, 100) for _ in range(10)]
 
 def bubble_sort():
+    global comparison
+    global transfer
     for i in range(9):
         for j in range(9 - i):
+            comparison += 1
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                transfer += 1
 
 def select_sort():
     for i in range(len(arr) - 1):
@@ -28,9 +36,11 @@ def shaker_sort():
         left += 1
         
 print('original array\n', *arr)
-#bubble_sort()    
-#print('bubble sort\n', *arr)
+bubble_sort()    
+print('bubble sort\n', *arr)
+print('comparisons: ', comparison)
+print('transfers: ', transfer)
 #select_sort()               
 #print('selection sort\n', *arr)
-shaker_sort()               
-print('shaker sort\n', *arr) 
+#shaker_sort()               
+#print('shaker sort\n', *arr) 
