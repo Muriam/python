83a50eac2fc87ef25c6fc8f6bdd1cb267984d132
Maria Duvanova <d_muriam@inbox.ru>
2019-04-17 19:03:46 +0700

    functions are no longer as procedures

--- simple/Lab1_saod.py
+++ simple/Lab1_saod.py
@@ -1,32 +1,33 @@
 # отсортировать список: пузырьковой, методом прямого выбора, и шейкерной сортировками. Подчситать кол-во сравнений и пересылок.
 from random import randint
 
-comparison = 0
-transfer = 0
-comparison2 = 0
-transfer2 = 0
-
 arr = [randint(1, 100) for _ in range(10)]
+arr2 = arr.copy()        # создание копии списка, чтоб selection_sort происходила повторно с такими же числами, как было у bubble_sort
 size = len(arr)
 
-def bubble_sort():
-    global comparison                                   # сравнений
-    global transfer                                     # пересылок
+def bubble_sort(comparison=0, transfer=0):
     for i in range(9):
         for j in range(9 - i):
-            comparison += 1
+            comparison += 1                         # подсчтет сравнений
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                transfer += 1
-
-def select_sort():
-    global comparison2                                  
-    global transfer2
-    for i in range(size - 1):
-        for j in range(i + 1, size):
-            if arr[j] < arr[i]:
-                arr[i], arr[j] = arr[j], arr[i]
-    comparison2 = (int(((size * size) - size) / 2));            
+                transfer += 1                       # подсчет пересылок
+    return comparison, transfer
+        
+def select_sort(comparison2=0, transfer2=0):
+    for i in range(size-1):
+        min = i
+        for j in range(i+1, size):
+            if (arr2[j] < arr2[min]):
+                min = j
+        if (min == i): 
+            continue
+        temp = arr2[i]
+        arr2[i] = arr2[min]
+        arr2[min] = temp
+        transfer2 += 1                                 
+    comparison2 = (int(((size * size) - size) / 2))
+    return comparison2, transfer2          
 
 def shaker_sort():
     left = 0
@@ -43,12 +44,12 @@ def shaker_sort():
         
 print('\noriginal array\n', *arr)
 
-bubble_sort()    
+comparison, transfer = bubble_sort()    
 print('\nbubble sort\n', *arr)
 print('comparisons: ', comparison)
 print('transfers: ', transfer)
 
-select_sort()               
+comparison2, transfer2 = select_sort()               
 print('\nselection sort\n', *arr)
 print('comparisons: ', comparison2)
 
