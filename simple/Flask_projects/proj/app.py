4bac176a8f13ae7402d187cf9df8db954b54185b
Maria Duvanova <d_muriam@inbox.ru>
2021-04-11 19:08:45 +0700

    'test'

---
+++ simple/Flask_projects/proj/app.py
@@ -0,0 +1,12 @@
+#вывод текста сюда http://127.0.0.1:5000/
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def func():
+	return 'Tasmania is a beautiful island.'
+
+if __name__ == '__main__':
+    app.run()
