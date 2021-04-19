6ad060d71a8b2ba9efcb733577a268e2a12a5cd7
Maria Duvanova <d_muriam@inbox.ru>
2021-04-19 19:38:35 +0700

    add menu

--- simple/Flask_projects/proj/main.py
+++ simple/Flask_projects/proj/main.py
@@ -1,5 +1,3 @@
-#http://127.0.0.1:5000/
-#размещение html и css и прочего
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
@@ -7,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def func():
-    return render_template("index.html")
+    return render_template("index.html", name="Австралия")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
