f0ed9ec47cb95e68770ab3e2b903e06a60709bfc
Maria Duvanova <d_muriam@inbox.ru>
2021-05-01 09:45:34 +0700

    adding a menu template

--- simple/Flask_projects/proj/main.py
+++ simple/Flask_projects/proj/main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def func():
-    return render_template("index.html", name="Австралия")
+    d = {'Населенные пункты': 'Хобарт', 'Флора': 'Молочай тирукалли', 'Фауна': 'Тонкоклювый буревестник'}
+    return render_template("index.html", name="Австралия", d=d)
 
 
 if __name__ == '__main__':
