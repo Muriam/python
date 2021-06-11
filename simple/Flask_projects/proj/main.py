22d6b95090f03857947cd601f8bfd42f4e8759a1
Maria Duvanova <d_muriam@inbox.ru>
2021-06-11 17:24:22 +0700

    edit

--- simple/Flask_projects/proj/main.py
+++ simple/Flask_projects/proj/main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+db = SQLAlchemy
 
 
 @app.route('/')
 def func():
-    d = {'Населенные пункты': 'Хобарт', 'Флора': 'Молочай тирукалли', 'Фауна': 'Тонкоклювый буревестник'}
-    return render_template("index.html", name="Австралия", d=d)
+    return render_template("index.html", name="Россия")
 
 
 @app.route('/two')
 def func_two():
     return render_template("two.html")
+
+
+@app.route('/fauna')
+def func_three():
+    return render_template("fauna.html")
 
 
 if __name__ == '__main__':
