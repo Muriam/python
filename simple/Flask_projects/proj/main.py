2bb9791f9336e32bd11815cbf1a72c576ce2c7d9
Maria Duvanova <d_muriam@inbox.ru>
2021-06-20 21:45:37 +0700

    adding an entry to the database

--- simple/Flask_projects/proj/main.py
+++ simple/Flask_projects/proj/main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataBase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Insects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+
+    def __repr__(self):
+        return '<Insects %r>' % self.id
 
 
 @app.route('/')
@@ -16,9 +25,21 @@ def func_two():
     return render_template("two.html")
 
 
-@app.route('/fauna')
+@app.route('/fauna', methods=['POST', 'GET'])
 def func_three():
-    return render_template("fauna.html")
+    if request.method == 'POST':
+        name = request.form['name']
+
+        insects = Insects(name=name)
+
+        try:
+            db.session.add(insects)
+            db.session.commit()
+            return redirect('/fauna')
+        except:
+            return "не получилось добавить запись"
+    else:
+        return render_template("fauna.html")
 
 
 if __name__ == '__main__':
