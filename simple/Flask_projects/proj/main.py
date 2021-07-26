82a6fc8455de3eacd8e903f56ed8fb310ce9611a
Maria Duvanova <d_muriam@inbox.ru>
2021-07-26 22:07:22 +0700

    more tables

--- simple/Flask_projects/proj/main.py
+++ simple/Flask_projects/proj/main.py
@@ -3,23 +3,63 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db3.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flora.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Kinds(db.Model):
-    __tablename__ = 'kinds'
+class Kingdom(db.Model):
+    __tablename__ = 'kingdom'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    dragon = db.relationship('Dragonflies', backref='family')
+    name = db.Column(db.String(40))
+    with_plants = db.relationship('Plants', backref='k')
 
 
-class Dragonflies(db.Model):
-    __tablename__ = 'dragonflies'
+class Division(db.Model):
+    __tablename__ = 'division'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    family_id = db.Column(db.Integer, db.ForeignKey('kinds.id'))
+    name = db.Column(db.String(40))
+    with_plants = db.relationship('Plants', backref='d')
+
+
+class Class(db.Model):
+    __tablename__ = 'class'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    with_plants = db.relationship('Plants', backref='c')
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    with_plants = db.relationship('Plants', backref='o')
+
+
+class Family(db.Model):
+    __tablename__ = 'family'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    with_plants = db.relationship('Plants', backref='f')
+
+
+class Genus(db.Model):
+    __tablename__ = 'genus'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    with_plants = db.relationship('Plants', backref='g')
+
+
+class Plants(db.Model):
+    __tablename__ = 'plants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    g_id = db.Column(db.Integer, db.ForeignKey('genus.id'))
+    f_id = db.Column(db.Integer, db.ForeignKey('family.id'))
+    o_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    c_id = db.Column(db.Integer, db.ForeignKey('class.id'))
+    d_id = db.Column(db.Integer, db.ForeignKey('division.id'))
+    k_id = db.Column(db.Integer, db.ForeignKey('kingdom.id'))
 
 
 @app.route('/')
@@ -32,10 +72,10 @@ def func_two():
     return render_template("two.html")
 
 
-@app.route('/fauna')
+@app.route('/flora')
 def data_base():
-    data = Dragonflies.query.filter(Dragonflies.family_id.like(2))
-    return render_template("fauna.html", data=data)
+    data = Plants.query.filter(Plants.f_id.like(1))
+    return render_template("flora.html", data=data)
 
 
 if __name__ == '__main__':
