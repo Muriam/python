a07fe99d4411021dd7ce782267176375207bb030
Maria Duvanova <d_muriam@inbox.ru>
2021-08-11 00:07:17 +0700

    test

--- simple/firstFile.py
+++
@@ -1,9 +0,0 @@
-#чтение файла и вывод на экран
-try:
-    f = open('text.txt', 'a')
-    while(True):
-        f.write(input())
-        f.write('\n')
-except KeyboardInterrupt:
-    f.write('\n')
-    f.close()
