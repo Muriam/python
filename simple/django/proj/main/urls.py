425066099d8baf21f21a3cde0b85cc66bda0325f
Maria Duvanova <d_muriam@inbox.ru>
2021-12-07 16:26:48 +0700

    templates inheritance

--- simple/django/proj/main/urls.py
+++ simple/django/proj/main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts),                # список url-ов
-    path('eng_1/', grammar),
-    path('eng_2/', dictionary),
-    path('library/', books),
-    path('framework_1/', flask),
-    path('framework_2/', django),
+    path('', posts, name='home'),                # список url-ов
+    path('eng_1/', grammar, name='grammar'),     # name - прописаны для отображения меню (base.html)
+    path('eng_2/', dictionary, name='dictionary'),
+    path('library/', books, name='books'),
+    path('framework_1/', flask, name='flask'),
+    path('framework_2/', django, name='django'),
 ]
