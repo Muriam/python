a98589927675c120834d82fffe23da85881c2a96
Maria Duvanova <d_muriam@inbox.ru>
2022-03-20 13:28:13 +0700

    edit list.py

--- simple/list.py
+++ simple/list.py
@@ -1,11 +1,12 @@
-s = list(map(int, input().split()))
-n = len(s)
-x = []
+# найти минимальный положительный элемент списка (значение которого > 0)
+arr = list(map(int, input().split()))
+n = len(arr)
+arr2 = []
 
 for i in range(n):
-    if s[i] > 0:
-        x.append(s[i])
-if x:
-    print(min(x))
+    if arr[i] > 0:
+        arr2.append(arr[i])
+if arr2:                    # если список2 не пуст
+    print('минимальный положительный элемент = ', min(arr2))
 else:
     print('Empty')
