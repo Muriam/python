d17204b15ba1addcfd023fd5fc3a41c3bb2e7af5
Maria Duvanova <d_muriam@inbox.ru>
2024-12-10 02:24:13 +0700

    add f_string_v3

---
+++ simple/f_string_v3.py
@@ -0,0 +1,7 @@
+a, b = int(input("первое число = ")), int(input("второе число = "))
+
+result1 = (a + b) ** 2
+result2 = a ** 2 + b ** 2
+
+print(f'Квадрат суммы {a} и {b} равен {result1}')
+print(f'Сумма квадратов {a} и {b} равна {result2}')
