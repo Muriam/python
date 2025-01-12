84ca00e5024c1ebd0ad7ea19481515ae4a516bda
Maria Duvanova <d_muriam@inbox.ru>
2025-01-13 01:16:57 +0700

    edit file

--- simple/generator.py
+++ simple/generator.py
@@ -1,5 +1,6 @@
-n1, n2, n3, n4, n5 = float(input()), float(input()), float(input()), float(input()), float(input())
+# n1, n2, n3, n4, n5 = float(input()), float(input()), float(input()), float(input()), float(input())
+# numbers = [n1, n2, n3, n4, n5]
+numbers = [float(input()) for _ in range(5)]
 
-numbers = [n1, n2, n3, n4, n5]
-res = sum(abs(num) for num in numbers)          # Сумма абсолютных значений
+res = sum(abs(num) for num in numbers)              # Сумма абсолютных значений
 print(res)
