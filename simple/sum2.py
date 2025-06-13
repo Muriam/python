b046a24956311814f8644b995a3f0cfc66ec6e1e
Maria Duvanova <d_muriam@inbox.ru>
2025-06-13 09:55:24 +0700

    add file

---
+++ simple/sum2.py
@@ -0,0 +1,11 @@
+n = int(input())                  # количество чисел
+
+lst = []
+
+for _ in range(n):
+    lst.append(int(input()))      # добавление каждого числа в список
+
+print(sum(lst))
+
+
+
